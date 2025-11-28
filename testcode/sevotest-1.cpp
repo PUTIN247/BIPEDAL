f60352ff28c724e156aa33df28288b43c8c1092f
Prashant <meenaprashant247c@gmail.com>
2025-11-28 18:50:18 +0530

    Update sevotest-1.cpp

--- testcode/sevotest-1.cpp
+++ testcode/sevotest-1.cpp
@@ -6,14 +6,14 @@ int servoPin = 18;       // GPIO pin the servo signal is on
 int angle = 20;          // current angle (degrees)
 int direction = 1;       // +1 = increasing angle, -1 = decreasing angle
 int minAngle = 20;       // lower bound (degrees)
-int maxAngle = 160;      // upper bound (degrees)
-int stepDelay = 80;      // ms delay between steps (controls speed)
+int maxAngle = 90;      // upper bound (degrees)
+int stepDelay = 100;      // ms delay between steps (controls speed)
 
 void setup() {
   Servo::useTimer(0);          // select hardware timer 0 for PWM (must before attach)
   myservo.attach(servoPin);    // attach servo to pin (start PWM output)
   myservo.write(angle);        // set initial servo position (20°)
-  delay(500);                  // wait 500 ms so servo reaches initial position
+  delay(2000);                  // wait 500 ms so servo reaches initial position
 }
 
 void loop() {
